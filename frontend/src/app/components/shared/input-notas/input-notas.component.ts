2ba3de3e35a0485b0a458b4c0a1e63f2250adc60
AdrianDiaz24 <adriandiazangulo23@gmail.com>
2026-02-10 09:47:54 +0100

    crear segundo componente

---
+++ frontend/src/app/components/shared/input-notas/input-notas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-notas',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-notas.component.html',
+  styleUrl: './input-notas.component.scss'
+})
+export class InputNotasComponent {
+
+}
--- frontend/src/pages/notas/notas.component.ts
+++ frontend/src/pages/notas/notas.component.ts
@@ -1,9 +1,12 @@
 import {Component, input} from '@angular/core';
+import {InputNotasComponent} from "../../app/components/shared/input-notas/input-notas.component";
 
 @Component({
   selector: 'app-notas',
   standalone: true,
-  imports: [],
+  imports: [
+    InputNotasComponent
+  ],
   templateUrl: './notas.component.html',
   styleUrl: './notas.component.scss'
 })
