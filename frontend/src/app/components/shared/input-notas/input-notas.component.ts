dafbca66ff963f933400a2723a2e20e1965c5dcd
AdrianDiaz24 <adriandiazangulo23@gmail.com>
2026-02-10 10:31:10 +0100

    crear grid

--- frontend/src/app/components/shared/input-notas/input-notas.component.ts
+++ frontend/src/app/components/shared/input-notas/input-notas.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-input-notas',
   standalone: true,
-  imports: [],
+    imports: [
+        NgForOf,
+        NgIf
+    ],
   templateUrl: './input-notas.component.html',
   styleUrl: './input-notas.component.scss'
 })
